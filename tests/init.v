28f93c18454b20058729a05dfa0b19181a00545c
Gaëtan Gilbert <gaetan.gilbert@skyskimmer.net>
2022-06-15 17:42:27 +0200

    redirects in init test

--- tests/init.v
+++ tests/init.v
@@ -1,12 +1,12 @@
 From LeanImport Require Import Lean.
 
-Lean Import "../dumps/init" 1 414836.
+Redirect "init1.log" Lean Import "../dumps/init" 1 414836.
 
-Fail Lean Import "../dumps/init" 414836 414837.
+Redirect "init2.log" Fail Lean Import "../dumps/init" 414836 414837.
 (* This fails because it expects to reduce a well-founded fixpoint in conversion mode,
    but the principal argument is Irrelevant so skip_irrelevant_stack does nonsense
    (mildly not a Coq bug since the fixpoint can only be defined
    thanks to acc being declared with unchecked univs)
 *)
 
-Lean Import "../dumps/init" 414837.
+Redirect "init3.log" Lean Import "../dumps/init" 414837.
