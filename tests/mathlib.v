496ff371b56abea93afc521b48308afc7a531f42
Gaëtan Gilbert <gaetan.gilbert@skyskimmer.net>
2023-01-27 15:45:45 +0100

    add more test files (not enabled)

---
+++ tests/mathlib.v
@@ -0,0 +1,5 @@
+From LeanImport Require Import Lean.
+
+Set Lean Error Mode "Stop".
+
+Time Redirect "mathlib.log" Lean Import "/home/gaetan/dev/lean/library/mathlib.out".
---
+++ tests/stdlib.v
@@ -0,0 +1,5 @@
+From LeanImport Require Import Lean.
+
+Set Lean Error Mode "Skip".
+
+Redirect "stdlib.log" Lean Import "../dumps/stdlib".
