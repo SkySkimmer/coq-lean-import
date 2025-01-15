1585aab427b9d49d4259bdb9123c09339e0a36ef
Jason Gross <jgross@mit.edu>
2025-01-15 15:54:10 +0000

    No need for upfront instantiation

--- tests/anomaly_print_projections.v
+++ tests/anomaly_print_projections.v
@@ -1,5 +1,4 @@
 From LeanImport Require Import Lean.
-Set Lean Upfront Instantiation.
 Redirect "anomaly_print_projections.log" Lean Import "../dumps/anomaly_print_projections.out".
 Redirect "anomaly_print_projections.Print.aexp.log" Print aexp.
 Redirect "anomaly_print_projections.Print.aeval.log" Print aeval.
