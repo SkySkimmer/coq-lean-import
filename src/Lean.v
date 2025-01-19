c4776416d5e6105772369e2a6a5697f14197480f
Jason Gross <jgross@mit.edu>
2025-01-19 16:37:58 +0000

    Add more infra for prim string

--- src/Lean.v
+++ src/Lean.v
@@ -1,4 +1,4 @@
-From Coq Require ZArith Lia ZifyBool.
+From Stdlib Require ZArith Lia ZifyBool Uint63.
 Declare ML Module "coq-lean-import.plugin".
 
 Set Universe Polymorphism.
@@ -108,7 +108,13 @@ Definition Nat_pred (n : Nat) : Nat :=
   | Nat_succ n => n
   end. *)
 
-Variant InvalidChar (n : Nat) : SProp := invalid_char.
+Variant Or@{} (a a0 : SProp) : SProp :=
+| Or_inl : a -> Or a a0
+| Or_inr : a0 -> Or a a0.
+Register Or as lean.Or.
+Record And@{} (a a0 : SProp) : SProp := And_intro
+  { left : a;  right : a0 }.
+Register And as lean.And.
 
 Inductive sEmpty : SProp := .
 
@@ -212,10 +218,33 @@ End nat_notation.
 Add Zify InjTyp Inj_Nat_Z.
 
 Number Notation Nat Nat_of_num_uint Nat_to_num_uint (abstract after 5000) : Nat_scope.
+(* Tell the kernel to unfold these wrappers early, to speed things up *)
+#[global] Strategy -10000 [Nat_of_num_uint Nat_to_num_uint].
+
+#[local] Set Warnings "-abstract-large-number".
+Definition UInt32_size : Nat := 0x100000000%Nat.
+Register UInt32_size as lean.UInt32_size.
+
+Record Fin@{} (n : Nat) := Fin_mk { val : Nat; isLt : (val < n)%Nat }.
+Register Fin as lean.Fin.
+Record UInt32@{} := UInt32_mk { val0 : Fin UInt32_size }.
+Register UInt32 as lean.UInt32.
+
 
 Section strings.
   Import ZArith NArith Lia Zify ZifyBool.
+  Variant InvalidUInt32 (n : N) : Set := invalid_uint32.
+  Variant InvalidChar (n : N) : Set := invalid_char.
   #[local] Set Warnings "-abstract-large-number".
+
+  Lemma Nat_lt_to_N (n : N) (m : N) : (n <? m)%N = true -> Nat_of_N n < Nat_of_N m.
+  Proof.
+    cbv [N_of_Nat Nat_of_N].
+    intro H; apply nat_le_Nat_le.
+    cbn [nat_of_Nat].
+    rewrite ?nat2Natid.
+    lia.
+  Qed.
 
   Lemma Nat_lt_to_N_l (n : Nat) (m : N) : (N_of_Nat n <? m)%N = true -> n < Nat_of_N m.
   Proof.
@@ -235,44 +264,120 @@ Section strings.
     lia.
   Qed.
 
-  Section with_or.
-    Context (Or : SProp -> SProp -> SProp)
+  (* Section with_or. *)
+    (* Context (Or : SProp -> SProp -> SProp)
             (Or_inl : forall P Q, P -> Or P Q)
             (Or_inr : forall P Q, Q -> Or P Q)
             (And : SProp -> SProp -> SProp)
-            (And_intro : forall P Q, P -> Q -> And P Q).
-    #[local] Infix "\/" := Or : type_scope.
-    #[local] Infix "/\" := And : type_scope.
-    #[local] Open Scope Nat_scope.
+            (And_intro : forall P Q, P -> Q -> And P Q). *)
+  #[local] Set Warnings "-notation-overridden".
+  #[local] Infix "\/" := Or : type_scope.
+  #[local] Infix "/\" := And : type_scope.
+  #[local] Open Scope Nat_scope.
 
-    Definition check_isValidChar (n : Nat) : bool
-      := let n := N_of_Nat n in
-         ((n <? 0xd800) || ((0xdfff <? n) && (n <? 0x110000)))%N%bool.
+  Definition Nat_isValidChar (n : Nat) : SProp
+    := n < 0xd800 \/ (0xdfff < n /\ n < 0x110000).
 
-    Definition make_isValidChar (n : Nat)
-      : if check_isValidChar n
-        then n < 0xd800 \/ (0xdfff < n /\ n < 0x110000)
-        else InvalidChar n.
-    Proof.
-      cbv [check_isValidChar].
-      pose proof (Nat_lt_to_N_l n 0xd800) as H1.
-      pose proof (Nat_lt_to_N_r 0xdfff n) as H2.
-      pose proof (Nat_lt_to_N_l n 0x110000) as H3.
-      destruct N.ltb; cbn [orb];
-      [ | destruct N.ltb; cbn [andb]; [ | constructor ] ];
-      [ | destruct N.ltb; cbn [andb]; [ | constructor ] ];
-      [ apply Or_inl, nat_le_Nat_le; revert H1 | apply Or_inr, And_intro; apply nat_le_Nat_le; [ revert H2 | revert H3 ] ];
-      clear.
-      all: cbv [Nat_of_N Nat_of_num_uint].
-      all: intro H; specialize (H Logic.eq_refl).
-      all: apply Nat_le_nat_le' in H.
-      1: etransitivity; [ exact H | ].
-      2: etransitivity; [ | exact H ].
-      3: etransitivity; [ exact H | ].
-      all: clear.
-      all: zify; clear.
-      all: cbn [nat_of_Nat]; rewrite ?nat2Natid, ?Nat2Z.inj_succ, ?N_nat_Z.
-      all: vm_compute; congruence.
-    Qed.
-  End with_or.
+  Record Char@{} := Char_mk
+  { val1 : UInt32; valid : Nat_isValidChar val1.(val0).(val _) }.
+
+  Definition check_N_isValidChar (n : N) : bool
+    := ((n <? 0xd800) || ((0xdfff <? n) && (n <? 0x110000)))%N%bool.
+
+  Definition Fin_mk_N (n : N) (val : N) (isLt : (val <? n)%N = true) : Fin (Nat_of_N n)
+    := Fin_mk (Nat_of_N n) (Nat_of_N val) (Nat_lt_to_N val n isLt).
+
+  Definition UInt32_mk_N (val : N) (isLt : (val <? 0x100000000)%N = true) : UInt32
+    := UInt32_mk (Fin_mk_N 0x100000000 val isLt).
+
+  Lemma Nat_isValidChar_mk_N (n : N) (isLt : check_N_isValidChar n = true)
+    : Nat_isValidChar (Nat_of_N n).
+  Proof.
+    cbv [Nat_isValidChar Nat_of_num_uint check_N_isValidChar] in *.
+    pose proof (Nat_lt_to_N n 0xd800) as H1.
+    pose proof (Nat_lt_to_N 0xdfff n) as H2.
+    pose proof (Nat_lt_to_N n 0x110000) as H3.
+    destruct N.ltb; cbn [orb] in *;
+    [ | destruct N.ltb; cbn [andb] in *; [ | exfalso; congruence ] ];
+    [ | destruct N.ltb; cbn [andb] in *; [ | exfalso; congruence ] ];
+    [ apply Or_inl, nat_le_Nat_le; revert H1 | apply Or_inr, And_intro; apply nat_le_Nat_le; [ revert H2 | revert H3 ] ];
+    clear.
+    all: cbv [Nat_of_N Nat_of_num_uint].
+    all: intro H; specialize (H Logic.eq_refl).
+    all: apply Nat_le_nat_le' in H.
+    1: etransitivity; [ exact H | ].
+    2: etransitivity; [ | exact H ].
+    3: etransitivity; [ exact H | ].
+    all: clear.
+    all: zify; clear.
+    all: cbn [nat_of_Nat]; rewrite ?nat2Natid, ?Nat2Z.inj_succ, ?N_nat_Z.
+    all: vm_compute; congruence.
+  Qed.
+
+  Definition Char_mk_N (val : N) (isLt : ((val <? 0x100000000)%N && check_N_isValidChar val)%bool = true) : Char
+    := Char_mk (UInt32_mk_N val (proj1 (andb_prop _ _ isLt))) (Nat_isValidChar_mk_N val (proj2 (andb_prop _ _ isLt))).
+
+  Definition reflective_Char_mk (val : N)
+    : if ((val <? 0x100000000)%N && check_N_isValidChar val)%bool
+      then Char
+      else InvalidChar val
+    := let isLt := ((val <? 0x100000000)%N && check_N_isValidChar val)%bool in
+       match isLt return ((val <? 0x100000000)%N && check_N_isValidChar val)%bool = isLt -> if isLt then Char else InvalidChar val with
+       | true => fun H => Char_mk_N val H
+       | false => fun _ => invalid_char val
+       end Logic.eq_refl.
+
+  Definition reflective_Char_mk_prim (val : Uint63.int)
+    := reflective_Char_mk (Z.to_N (Uint63.to_Z val)).
+
+
+  (* Definition reflective_UInt32_mk (val : N) : if (val <? 0x100000000)%N
+                                              then UInt32
+                                              else InvalidUInt32 val
+    := let isLt := (val <? 0x100000000)%N in
+       match isLt return (val <? 0x100000000)%N = isLt -> if isLt then UInt32 else InvalidUInt32 val with
+       | true => fun H => UInt32_mk_N val H
+       | false => fun _ => invalid_uint32 val
+       end Logic.eq_refl.
+
+  Definition reflective_UInt32_mk_prim (val : Uint63.int)
+    := reflective_UInt32_mk (Z.to_N (Uint63.to_Z val)).
+
+
+  Definition check_isValidChar (n : Nat) : bool
+    := let n := N_of_Nat n in
+        ((n <? 0xd800) || ((0xdfff <? n) && (n <? 0x110000)))%N%bool.
+
+  Definition reflective_isValidChar_mk (n : Nat)
+    : if check_isValidChar n
+      then n < 0xd800 \/ (0xdfff < n /\ n < 0x110000)
+      else InvalidChar n.
+  Proof.
+    cbv [check_isValidChar].
+    pose proof (Nat_lt_to_N_l n 0xd800) as H1.
+    pose proof (Nat_lt_to_N_r 0xdfff n) as H2.
+    pose proof (Nat_lt_to_N_l n 0x110000) as H3.
+    destruct N.ltb; cbn [orb];
+    [ | destruct N.ltb; cbn [andb]; [ | constructor ] ];
+    [ | destruct N.ltb; cbn [andb]; [ | constructor ] ];
+    [ apply Or_inl, nat_le_Nat_le; revert H1 | apply Or_inr, And_intro; apply nat_le_Nat_le; [ revert H2 | revert H3 ] ];
+    clear.
+    all: cbv [Nat_of_N Nat_of_num_uint].
+    all: intro H; specialize (H Logic.eq_refl).
+    all: apply Nat_le_nat_le' in H.
+    1: etransitivity; [ exact H | ].
+    2: etransitivity; [ | exact H ].
+    3: etransitivity; [ exact H | ].
+    all: clear.
+    all: zify; clear.
+    all: cbn [nat_of_Nat]; rewrite ?nat2Natid, ?Nat2Z.inj_succ, ?N_nat_Z.
+    all: vm_compute; congruence.
+  Qed.
+  End with_or. *)
+
+
 End strings.
+
+Register Nat_isValidChar as lean.Nat_isValidChar.
+Register Char as lean.Char.
+Register reflective_Char_mk_prim as lean.Char.mk.reflective_prim.
--- tests/core.v
+++ tests/core.v
@@ -6,3 +6,19 @@ Redirect "core1.log" Lean Import "../dumps/core" 1 5584.
 Fail Redirect "core2.log" Lean Import "../dumps/core" 5584 63566.
 
 (* Redirect "core3.log" Lean Import "../dumps/core" 5086 63566. *)
+
+Set Printing Universes.
+Print String.
+Print Char.
+Print UInt32.
+Eval hnf in LT_lt_inst1 Nat instLTNat.
+Print Nat_le.
+Print Or.
+Print And.
+Print Fin.
+Print UInt32_isValidChar.
+Print Nat_isValidChar.
+Print UInt32_toNat.
+(* Compute Z.of_nat (nat_of_Nat UInt32_size. *)
+(* n < 0xd800 ∨ (0xdfff < n ∧ n < 0x110000) *)
+(* Eval hnf in UInt32_isValidChar _. *)
